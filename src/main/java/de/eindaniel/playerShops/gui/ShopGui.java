4f8f4a8ab3eb1cf565ecc09d375e7360c61cbac6
Daniel <daniel.dalol@icloud.com>
2026-05-31 16:03:10 +0200

    fix ShopGui to use the correct config reference for currency symbol

--- src/main/java/de/eindaniel/playerShops/gui/ShopGui.java
+++ src/main/java/de/eindaniel/playerShops/gui/ShopGui.java
@@ -45,7 +45,7 @@ public class ShopGui implements InventoryHolder {
             bm.displayName(MM.deserialize(Main.get().i18n().get("shopGui.buyItem.title"))
                     .decoration(TextDecoration.ITALIC, false));
             bm.lore(List.of(MM.deserialize(Main.get().i18n().get("shopGui.buyItem.lore",
-                    String.format("%.2f" + plugin.config().get("economy.currency-symbol", "$"), shop.getBuyPrice())))
+                    String.format("%.2f" + Main.get().config().get("economy.currency-symbol", "$"), shop.getBuyPrice())))
                     .decoration(TextDecoration.ITALIC, false)));
             buy.setItemMeta(bm);
             inv.setItem(11, buy);
@@ -57,7 +57,7 @@ public class ShopGui implements InventoryHolder {
             sm.displayName(MM.deserialize(Main.get().i18n().get("shopGui.sellItem.title"))
                     .decoration(TextDecoration.ITALIC, false));
             sm.lore(List.of(MM.deserialize(Main.get().i18n().get("shopGui.sellItem.lore",
-                    String.format("%.2f" + plugin.config().get("economy.currency-symbol", "$"), shop.getSellPrice())))
+                    String.format("%.2f" + Main.get().config().get("economy.currency-symbol", "$"), shop.getSellPrice())))
                     .decoration(TextDecoration.ITALIC, false)));
             sell.setItemMeta(sm);
             inv.setItem(15, sell);
